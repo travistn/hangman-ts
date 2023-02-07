e41a206671431528e3a33f3d99ac5ea0b36943dc
travistn <travisn714@gmail.com>
2023-02-07 14:18:15 -0800

    Set up states for the words

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
+
 import './App.css';
+import words from './wordList.json';
 
 const App = () => {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
   return <div></div>;
 };
 
